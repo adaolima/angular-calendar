5e997c40ab77fa2c9644b937b792af8498c9240d
Adão Lima <adaoolima@gmail.com>
2021-08-15 23:56:55 -0300

    Avoid temporaly many request

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,28 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 import { CalendarService } from './calendar.service'
 
 import { ICalendarEvents } from './calendar'
+import { LocalStorageService } from '../shared/local-storage.service';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss']
+  styleUrls: ['./calendar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CalendarComponent implements OnInit {
 
   eventsList: ICalendarEvents[]
-  id: number
+  calendarId: number;
 
-  constructor(private calendarService: CalendarService) { }
+  constructor(
+    private calendarService: CalendarService, 
+    private localStorageService: LocalStorageService
+  ) { }
 
   async ngOnInit() {
-    this.calendarService.getID().subscribe(({ data }) => {
-      this.calendarService.getEvents(data.id).subscribe(({ data }) => {
-        this.eventsList = data.items
-      })
-    })
-    
-  }
 
+    // Temporaly to not take to much requests
+    if(this.localStorageService.get('calendarId')) {
+      this.calendarId = this.localStorageService.get('calendarId')
+    } else {
+      this.calendarService.getID().subscribe(({ data }) => {
+        this.localStorageService.set('calendarId', data.id)
+        this.calendarId = data.id
+      })
+    }
+
+    // Temporaly to not take to much requests
+    if(this.localStorageService.get('eventsList')) {
+      this.eventsList = this.localStorageService.get('eventsList')
+    } else {
+      this.calendarService.getEvents(this.calendarId).subscribe(({ data }) => {
+        this.eventsList = data.items
+        this.localStorageService.set('eventsList', data.items)
+      })
+    }
+
+  }
 }
