70fa5cf613c779bc15f2012685166a816de43d53
Adão Lima <adaoolima@gmail.com>
2021-08-16 01:09:41 -0300

    Removed resource not being used

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -8,8 +8,7 @@ import { LocalStorageService } from '../shared/local-storage.service';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
 
